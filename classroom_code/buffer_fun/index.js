74178d1ca1bb0c53c1464213933521195a342b16
Damiano4321 <damiano1.castelli@gmail.com>
2024-10-30 15:56:09 +0000

    modified:   classroom_code/buffer_fun/index.html 	modified:   classroom_code/buffer_fun/index.js

--- classroom_code/buffer_fun/index.js
+++ classroom_code/buffer_fun/index.js
@@ -1,40 +1,37 @@
-
 const c = new AudioContext();
 
-function createsANoiseBuffer() {
-    const b = c.createBuffer(1, c.sampleRate * 2, 
-    c.sampleRate);
-
+function returnBufferData() {
+    const b = c.createBuffer(1, c.sampleRate * 2,
+        c.sampleRate);
     const audioData = b.getChannelData(0);
+    return b, audioData
+}
 
-    for(var i=0; i<audioData.length; i++) {
-        audioData[i] = Math.random();   
+function createsANoiseBuffer() {
+    const b, audioData = returnBufferData();
+
+    for (var i = 0; i < audioData.length; i++) {
+        audioData[i] = Math.random();
     }
     return b;
 }
 
 function createsASineBuffer(f) {
-        return function() {
-        const b = c.createBuffer(1, c.sampleRate * 2, 
-        c.sampleRate);
+    return function () {
+        const b, audioData = returnBufferData();
 
-        
-        alpha = Math.PI*2*f/c.sampleRate;
-        const audioData = b.getChannelData(0);
-
-        for(var i=0; i<audioData.length; i++) {
-            audioData[i] = Math.sin(alpha*i);   
+        alpha = Math.PI * 2 * f / c.sampleRate;
+        for (var i = 0; i < audioData.length; i++) {
+            audioData[i] = Math.sin(alpha * i);
         }
         return b;
     }
 }
 
-
-
 function playBuffer(bufferCreator) {
 
     b = bufferCreator();
- 
+
     const bs = c.createBufferSource();
     bs.buffer = b;
     bs.connect(c.destination);
@@ -42,6 +39,6 @@ function playBuffer(bufferCreator) {
 
 }
 
-function stop() {
+function stopSound() {
     c.suspend();
 }

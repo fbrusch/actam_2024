bd625973b15bd4955019802cea0bee7667976adb
fbrusch <fbrusch@users.noreply.github.com>
2024-10-30 15:37:15 +0000

    deploy: 4f6008708a8db130a0ed2d74279abf02203d7ec6

--- classroom_code/buffer_fun/index.js
+++ classroom_code/buffer_fun/index.js
@@ -1,12 +1,45 @@
 
 const c = new AudioContext();
 
-function main() {
+function createsANoiseBuffer() {
+    const b = c.createBuffer(1, c.sampleRate * 2, 
+    c.sampleRate);
 
-    const o = c.createOscillator();
-    o.connect(c.destination);
+    const audioData = b.getChannelData(0);
 
-    o.start();
+    for(var i=0; i<audioData.length; i++) {
+        audioData[i] = Math.random();   
+    }
+    return b;
+}
+
+function createsASineBuffer(f) {
+        return function() {
+        const b = c.createBuffer(1, c.sampleRate * 2, 
+        c.sampleRate);
+
+        
+        alpha = Math.PI*2*f/c.sampleRate;
+        const audioData = b.getChannelData(0);
+
+        for(var i=0; i<audioData.length; i++) {
+            audioData[i] = Math.sin(alpha*i);   
+        }
+        return b;
+    }
+}
+
+
+
+function playBuffer(bufferCreator) {
+
+    b = bufferCreator();
+ 
+    const bs = c.createBufferSource();
+    bs.buffer = b;
+    bs.connect(c.destination);
+    bs.start();
+
 }
 
 function stop() {

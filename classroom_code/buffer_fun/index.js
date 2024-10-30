7850bfaa20e77b8ed39c8f4a5053f130f8e8f026
fbrusch <fbrusch@gmail.com>
2024-10-30 14:48:33 +0000

    refactor some code

--- classroom_code/buffer_fun/index.js
+++ classroom_code/buffer_fun/index.js
@@ -1,12 +1,41 @@
 
 const c = new AudioContext();
 
-function main() {
+function createsANoiseBuffer() {
+    const b = c.createBuffer(1, c.sampleRate * 2, 
+    c.sampleRate);
 
-    const o = c.createOscillator();
-    o.connect(c.destination);
+    const audioData = b.getChannelData(0);
 
-    o.start();
+    for(var i=0; i<audioData.length; i++) {
+        audioData[i] = Math.random();   
+    }
+    return b;
+}
+
+function createsASineBuffer() {
+    const b = c.createBuffer(1, c.sampleRate * 2, 
+    c.sampleRate);
+
+    const audioData = b.getChannelData(0);
+
+    for(var i=0; i<audioData.length; i++) {
+        audioData[i] = Math.sin(i/10);   
+    }
+    return b;
+}
+
+
+
+function playBuffer(bufferCreator) {
+
+    b = bufferCreator();
+ 
+    const bs = c.createBufferSource();
+    bs.buffer = b;
+    bs.connect(c.destination);
+    bs.start();
+
 }
 
 function stop() {
